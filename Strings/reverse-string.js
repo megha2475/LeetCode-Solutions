6ed31f95ee10394b763bf8f3d65f6ae4ffa96090
megha2475 <meghaks2003@gmail.com>
2025-05-13 10:21:14 +0530

    Added reverse words in a string solution`

---
+++ Strings/reverse-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let words = s.split(' ');
+    let reverse = '';
+    for(let i=0;i<words.length;i++){
+        for(let j=words[i].length-1;j>=0;j--){
+            reverse += words[i][j];
+        }
+        if(i<words.length-1){
+            reverse += ' ';
+        }
+    }
+    return reverse;
+};
